797a12fb95e99c93304358c524a5b0968e12109d
leonardoclf <leonardoclf@gmail.com>
2021-03-15 15:02:02 -0300

    addclass comp details.js

--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,52 @@
-import { Component } from 'react';
+import { Component } from "react";
 import { withRouter } from 'react-router-dom';
 
+class Details extends Component {
+  constructor() {
+    super(); // call component constructor
 
-const Details extends Component {
-    
+    this.state = { loading: true };
+  }
+
+  // no momento da inicialização
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id${this.props.match.params.id}`
+    );
+    const json = await res.json();
+    this.setState(
+      Object.assign(
+        {
+          loading: false,
+        },
+        json.pets[this.props.match.params.id - 1]
+      )
+    );
+  }
+
+  render() {
+
+    if (this.state.loading) {
+        return <h2>loading..</h2>
+    }
+
+    const {animal, breed, city, state, description, name} = this.state;
+    return (
+        <div className="details">
+            <div>
+                <h1>{name}</h1>
+                <h2>{animal} - {breed} - {city}, {state}</h2>
+                <button>Adopt {name}</button>
+                <p>{description}</p>
+            </div>
+        </div>
+    )
+  }
 }
 
-export default Details;
+export default withRouter(Details);
+
+/*
+    class component
+    withRouter(Details) high order component. 
+*/
